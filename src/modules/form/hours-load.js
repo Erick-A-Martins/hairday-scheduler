6567449be91287881725e77282e46e79b11a8fab
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-07 08:39:08 -0300

    create list of schedule hours and structure of a schedule for the api

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,7 +1,13 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js";
+
+const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }){
+    // Limpa a lista de horarios
+    hours.innerHTML = "";
+    
     const opening = openingHours.map((hour) => {
         // Recupera somente a hora.
         const [scheduleHour] = hour.split(":");
@@ -16,5 +22,34 @@ export function hoursLoad({ date }){
         }
     });
 
-    console.log(opening)
+    // Renderiza os horarios
+    opening.forEach(({ hour, available }) => {
+        const li = document.createElement("li");
+
+        li.classList.add("hour");
+        li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+        li.textContent = hour;
+        
+        if(hour === "9:00") {
+            hourHeaderAdd("Manhã");
+        } else if(hour === "13:00") {
+            hourHeaderAdd("Tarde");
+        } else if(hour === "18:00") {
+            hourHeaderAdd("Noite");
+        }
+
+        hours.append(li);
+    });
+
+    // Adiciona evento de clique nos horarios disponiveis
+    hoursClick();
+};
+
+function hourHeaderAdd(title){
+    const header = document.createElement("li");
+    header.classList.add("hour-period");
+    header.textContent = title;
+
+    hours.append(header);
 }
