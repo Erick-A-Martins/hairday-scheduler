4ef2dcfa9b92543204dd61d00b945fe536cac5a1
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-07 09:04:54 -0300

    create service for fetch a schedule

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,7 @@ import { apiConfig } from "./api-config.js";
 
 export async function scheduleNew({ id, name, when }){
     try {
+        // Faz a requisicao para enviar os dados do agendamento
         await fetch(`${apiConfig.baseUrl}/schedules`, {
             method: 'POST',
             headers: {
@@ -10,6 +11,7 @@ export async function scheduleNew({ id, name, when }){
             body: JSON.stringify({ id, name, when }),
         });
 
+        // Exibe mensagem de agendamento realizado
         alert("Agendamento realizado com sucesso!");
     } catch(error) {
         alert("Não foi possível agendar. Tente novamente mais tarde.");
