2dd30458f01d62c4bbe12968828be70a9903968f
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-08 11:21:24 -0300

    create method delete to cancel a schedule

--- src/modules/schedules/show.js
+++ src/modules/schedules/show.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 // Seleciona as sessões manhã, tarde e noite.
 const periodMorning = document.getElementById("period-morning");
 const periodAfternoon = document.getElementById("period-afternoon");
-const periodNight = document.getElementById("period-afternoon");
+const periodNight = document.getElementById("period-night");
 
 export function schedulesShow({ dailySchedules }){
     try {
